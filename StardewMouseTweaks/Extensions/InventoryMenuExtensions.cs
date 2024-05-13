003f186f7b04e9ee50fa9606dd732b4125677421
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-05-13 03:24:47 +0100

    add `InventoryMenuExtensions`, primarily for `TryGetHoveredItemSlot`

---
+++ StardewMouseTweaks/Extensions/InventoryMenuExtensions.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace StardewMouseTweaks.Extensions;
+
+public static class InventoryMenuExtensions
+{
+    public static bool TryGetHoveredItemSlot(this InventoryMenu menu, ICursorPosition position, [NotNullWhen(true)] out InventorySlot? slot)
+    {
+        foreach (var candidateSlot in menu) {
+            if (!candidateSlot.ClickableComponent.ContainsPoint(position)) continue;
+
+            slot = candidateSlot;
+            return true;
+        }
+
+        slot = null;
+        return false;
+    }
+
+    public static InventorySlot GetItemSlot(this InventoryMenu menu, int slotIndex)
+    {
+        return new InventorySlot {
+            InventoryMenu = menu,
+            Index = slotIndex,
+        };
+    }
+
+    public static InventoryMenuEnumerator GetEnumerator(this InventoryMenu menu) => new InventoryMenuEnumerator(menu);
+
+    public readonly record struct InventorySlot
+    {
+        public required InventoryMenu InventoryMenu { get; init; }
+        public required int Index { get; init; }
+
+        public ClickableComponent ClickableComponent => InventoryMenu.inventory[Index];
+
+        public Item? Item {
+            get => InventoryMenu.actualInventory[Index];
+            set => InventoryMenu.actualInventory[Index] = value;
+        }
+    }
+
+    public struct InventoryMenuEnumerator : IEnumerator<InventorySlot>
+    {
+        private InventoryMenu _inventoryMenu;
+        private int _index;
+
+        internal InventoryMenuEnumerator(InventoryMenu inventoryMenu)
+        {
+            _inventoryMenu = inventoryMenu;
+            _index = -1;
+        }
+
+        public bool MoveNext()
+        {
+            _index += 1;
+            return _index < _inventoryMenu.capacity;
+        }
+
+        public void Reset() => _index = -1;
+
+        public InventorySlot Current {
+            get {
+                try {
+                    return _inventoryMenu.GetItemSlot(_index);
+                }
+                catch (IndexOutOfRangeException) {
+                    if (_index < 0)
+                        throw new InvalidOperationException("Cannot retrieve element before advancing position from initial state.");
+                    throw;
+                }
+            }
+        }
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose() { }
+    }
+}
