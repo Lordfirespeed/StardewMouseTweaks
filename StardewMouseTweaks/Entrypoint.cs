69f4c2eb214a30e84de5b7eeddf8aa7b0a4a7e74
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-05-13 01:24:39 +0100

    add mod entrypoint

---
+++ StardewMouseTweaks/Entrypoint.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using StardewModdingAPI;
+
+namespace StardewMouseTweaks;
+
+[UsedImplicitly]
+internal sealed class Entrypoint : Mod
+{
+    private DragOperationManager _dragOperationManager = null!;
+
+    /// <summary>
+    /// Entrypoint.
+    /// </summary>
+    /// <seealso cref="StardewValley.Menus.InventoryMenu"/>
+    /// <param name="helper"></param>
+    public override void Entry(IModHelper helper)
+    {
+        _dragOperationManager = new DragOperationManager(helper, Monitor);
+    }
+}
