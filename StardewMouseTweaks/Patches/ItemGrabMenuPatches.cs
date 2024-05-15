50bcaae17eab8578a052a1fd3afc7899f09c4263
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-05-15 16:40:13 +0100

    add patch to change behaviour when left-clicking last shipped item

--- StardewMouseTweaks/Patches/ItemGrabMenuPatches.cs
+++ StardewMouseTweaks/Patches/ItemGrabMenuPatches.cs
@@ -1,10 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 using HarmonyLib;
 using JetBrains.Annotations;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
-using StardewMouseTweaks.Extensions;
 using StardewValley;
 using StardewValley.Menus;
 using Rectangle = System.Drawing.Rectangle;
@@ -76,7 +76,85 @@ public class ItemGrabMenuPatches : IPatch
         matcher.Advance(20);
         matcher.Labels.Add(label);
 
-        Entrypoint.Monitor.LogInstructionsFrom(matcher, LogLevel.Info);
+        return matcher.InstructionEnumeration();
+    }
+
+    public static void LeftClickShippingBinSlot(ItemGrabMenu menu)
+    {
+        if (menu.heldItem is null) TakeItemFromSlot();
+        else PlaceItemInSlot();
+        return;
+
+        void PlaceItemInSlot()
+        {
+            menu.behaviorFunction(menu.heldItem, Game1.player);
+            menu.heldItem = null;
+        }
+
+        void TakeItemFromSlot()
+        {
+            if (Game1.getFarm().lastItemShipped is not { } lastItemShipped)
+                return;
+
+            Game1.getFarm().getShippingBin(Game1.player).Remove(lastItemShipped);
+
+            if (_helper is null) throw new InvalidOperationException("HUH?");
+            if (_helper.Input is null) throw new InvalidOperationException("what.");
+
+            if (_helper.Input.IsDown(SButton.LeftShift)) {
+                if (!Game1.player.addItemToInventoryBool(lastItemShipped)) {
+                    Game1.getFarm().getShippingBin(Game1.player).Add(lastItemShipped);
+                    return;
+                }
+
+                Game1.playSound("coin");
+                Game1.getFarm().lastItemShipped = null;
+                if (Game1.player.ActiveObject == null)
+                    return;
+                Game1.player.showCarrying();
+                Game1.player.Halt();
+                return;
+            }
+
+            Game1.playSound("coin");
+            Game1.getFarm().lastItemShipped = null;
+            menu.heldItem = lastItemShipped;
+        }
+    }
+
+    [HarmonyPatch(nameof(ItemGrabMenu.receiveLeftClick))]
+    [HarmonyTranspiler]
+    [UsedImplicitly]
+    public static IEnumerable<CodeInstruction> PatchLeftClickShippingBinSlot(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        var matcher = new CodeMatcher(instructions);
+
+        matcher.MatchEndForward(
+            new CodeMatch(OpCodes.Ldarg_0),
+            new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(ItemGrabMenu), nameof(ItemGrabMenu.shippingBin))),
+            new CodeMatch(instr => instr.opcode == OpCodes.Brfalse),
+            new CodeMatch(OpCodes.Ldarg_0),
+            new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(ItemGrabMenu), nameof(ItemGrabMenu.lastShippedHolder))),
+            new CodeMatch(OpCodes.Ldarg_1),
+            new CodeMatch(OpCodes.Ldarg_2),
+            new CodeMatch(OpCodes.Callvirt, AccessTools.Method(typeof(ClickableComponent), nameof(ClickableComponent.containsPoint), [typeof(int), typeof(int)])),
+            new CodeMatch(instr => instr.opcode == OpCodes.Brfalse)
+        );
+
+        var jumpToLabel = (Label)matcher.Operand;
+        matcher.Advance(1);
+
+        matcher.InsertAndAdvance(
+            new CodeInstruction(OpCodes.Ldarg_0),
+            new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ItemGrabMenuPatches), nameof(LeftClickShippingBinSlot))),
+            new CodeInstruction(OpCodes.Ret)
+        );
+
+        var deleteFromIndex = matcher.Pos;
+        matcher.MatchStartForward(
+            new CodeMatch(instr => instr.labels.Contains(jumpToLabel))
+        );
+        matcher.RemoveInstructionsInRange(deleteFromIndex, matcher.Pos - 1);
 
         return matcher.InstructionEnumeration();
     }
